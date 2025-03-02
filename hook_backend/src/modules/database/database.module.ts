03d449037e2a2c258e18fc31b8a59741d713c74d
d0ra-th3-3xpl0ra <90798127+d0ra-1h3-3xpl0ra@users.noreply.github.com>
2025-03-02 16:39:19 +0100

    feat: add database.module for init connection

--- hook_backend/src/modules/database/database.module.ts
+++ hook_backend/src/modules/database/database.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/../../**/*.entity{.ts,.js}'],
+        synchronize: configService.get('NODE_ENV') === 'development',
+        logging: configService.get('NODE_ENV') === 'development',
+        migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+        migrationsRun: true,
+      }),
+    }),
+  ],
+})
 export class DatabaseModule {}
