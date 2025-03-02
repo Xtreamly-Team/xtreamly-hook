2317ea203d64bafa0286829bfdb87e240dfc12a5
d0ra-th3-3xpl0ra <90798127+d0ra-1h3-3xpl0ra@users.noreply.github.com>
2025-03-02 17:59:27 +0100

    feat: improce ORM config

--- hook_backend/ormconfig.ts
+++ hook_backend/ormconfig.ts
@@ -1,17 +1,15 @@
 import { DataSource } from 'typeorm';
-import { config } from 'dotenv';
-
-config();
+import { env } from './src/config/env.config';
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: env.DB_HOST,
+  port: env.DB_PORT,
+  username: env.DB_USERNAME,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/modules/database/migrations/*{.ts,.js}'],
-  logging: process.env.NODE_ENV === 'development',
+  logging: env.NODE_ENV === 'development',
   synchronize: false,
 });
