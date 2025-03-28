8d13d7987fa8a0557610c1a04b99eb1032e535d6
pablomasior <pawel.masior@lotangroup.com>
2025-03-28 18:16:46 +0100

    dummy policy

--- ai_backend/src/policy.py
+++ ai_backend/src/policy.py
@@ -9,40 +9,43 @@ def execute_policy(user_id: str):
     df = Positions().get_active_positions(user_id)
     gmx_price = GMXPrices().get_signed_price("ETH")
     vol_status = get_volatility_state("ETH")["classification"]  # midvol
-    return gmx_price
-
-
+    out = {
+        'perp_close': False,
+        'perp_open': False,
+        }
+    return out
 # =============================================================================
-# # =============================================================================
-# # info_input = { # Amin - worth adding token name
-# #     'user_id': 2636221,
-# #     'token': 'ETH',
-# # }
-# # =============================================================================
-# # =============================================================================
-# # id: string;
-# # userId: string;
-# # tokenA: string;
-# # tokenB: string;
-# # amountA: number;
-# # amountB: number;
-# # lowerTick: number;
-# # upperTick: number;
-# # hedgeAmount: number;
-# # status: PositionStatus;
-# # uniswapPositionId?: string;
-# # gmxPositionId?: string;
-# # createdAt: Date;
-# # updatedAt: Date;
-# # =============================================================================
-# 
+# =============================================================================
+# info_input = { # Amin - worth adding token name
+#     'user_id': 2636221,
+#     'token': 'ETH',
+# }
+# =============================================================================
+# =============================================================================
+# id: string;
+# userId: string;
+# tokenA: string;
+# tokenB: string;
+# amountA: number;
+# amountB: number;
+# lowerTick: number;
+# upperTick: number;
+# hedgeAmount: number;
+# status: PositionStatus;
+# uniswapPositionId?: string;
+# gmxPositionId?: string;
+# createdAt: Date;
+# updatedAt: Date;
+# =============================================================================
+
 # # =============================================================================
 # # INPUTS
 # # =============================================================================
 # from datetime import datetime, timedelta
+# import pandas as pd
 # time_now = datetime.utcnow()
-# market_status = 'lowvol' # to fetch from API
-# 
+# vol_status = 'lowvol' # to fetch from API
+
 # pos = {}
 # pos['id'] = 'string'
 # pos['userId'] = 'string'
@@ -60,12 +63,12 @@ def execute_policy(user_id: str):
 # pos['updatedAt'] = datetime.utcnow()
 # #add
 # pos['gmxPositionStartedAt'] = datetime.utcnow()-timedelta(hours=13) #(for now at open but can be any other date)
-# 
+
 # df = pd.DataFrame([pos]) # assume the output always provides 1-row tbl
-# 
+
 # gmx_price = 3138.737087496404
 # p = gmx_price # we get market price from GMX, right?
-# 
+
 # # =============================================================================
 # # POLICY
 # # =============================================================================
@@ -74,30 +77,23 @@ def execute_policy(user_id: str):
 # if 'gmxPositionId' in pos: # check if perp currently opened (and not liquidated) to see if close
 #     perp_close = False
 #     perp_time = (time_now-pos['gmxPositionStartedAt']).total_seconds()/3600
-#     
+    
 #     perp_close = False
 #     perp_close = True if perp_time >= PERP_MAX_HOURS else perp_close # to check to expire
-#     if perp_close: 
-#         out = {
-#             'perp_close': perp_close,
-#             'perp_open': False,
-#             }
-#         
-#     else:
-#         out = {
-#             'perp_close': perp_close,
-#             'perp_open': False,
-#             }
+#     out = {
+#         'perp_close': perp_close,
+#         'perp_open': False,
+#         }
 # else: # if not open, we may open perp
 #     p_a = 1.0001**pos['lowerTick']
 #     p_b = 1.0001**pos['upperTick']
 #     p_mid = (p_a+p_b)/2
-# 
+
 #     perp_open = False
 #     perp_open = True if pos['createdAt'] == pos['updatedAt'] else perp_open # to check if we are just opening position
 #     perp_open = True if (p >= p_mid/1.01) and (p <= p_mid*1.01) else perp_open # to check price condition
 #     if perp_open:
-#         leverage = PERP_LEVERAGE[market_status]
+#         leverage = PERP_LEVERAGE[vol_status]
 #         collateral = pos['hedgeAmount'] 
 #         out = {
 #             'perp_close': False,
@@ -111,7 +107,7 @@ def execute_policy(user_id: str):
 #             'perp_close': False,
 #             'perp_open': perp_open,
 #             }
-#         
+        
 # print(out) # this is a signal for Amin!
 # # Panos shall we save a new record to DB??
-# =============================================================================
+# # =============================================================================
